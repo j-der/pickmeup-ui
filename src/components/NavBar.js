bd7e0b1d8f584a89e385b02c71af8949c85fd6da
Jasmine Der <jasmineder@gmail.com>
2016-02-25 22:42:21 -0500

    playing with MaterialUI components in the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,23 @@ import React from 'react';
 import PostRide from './PostRide';
 import NewUser from './NewUser';
 import LogInForm from './LogInForm';
+import Avatar from './Avatar';
 
 var NavBar = React.createClass({
 
   render: function(){
     return (
       <div className="nav-bar">
+        <div className="avatar">
+          <Avatar />
+        </div>
+        <div className="nav-buttons">
+
+        </div>
       	<PostRide />
         <NewUser />
         <LogInForm />
+
       </div>
     );
   }
