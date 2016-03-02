cdf790d3fd90667ca9eeb91c81e50cef434d9d21
Jasmine Der <jasmineder@gmail.com>
2016-03-01 21:09:20 -0500

    working version of splash page

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import NavBar from './NavBar'
+// import the css className
+import '../application.css'
 
 var SplashPage = React.createClass({
 
   render: function() {
     return (
       <div>
-        <NavBar />
-        <SearchBar />
+      <div className="splash-image"><NavBar />
+        <SearchBar /></div>
+
 
       </div>
+
     );
   }
 
